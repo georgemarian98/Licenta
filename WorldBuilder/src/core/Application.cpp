b7f10d527fc8ff2623dce00543c2e0816fffd448
georgemarian98 <georgemarian98@gmail.com>
2021-04-12 23:36:53 +0300

    enable dock spac and sketch the menubar

--- WorldBuilder/src/core/Application.cpp
+++ WorldBuilder/src/core/Application.cpp
@@ -1,7 +1,6 @@
 #include "pch.h"
 
 #include "Application.h"
-
 #include "Renderer/Model.h"
 
 std::ostream& operator<<(std::ostream& g, glm::vec3 vec)
@@ -28,7 +27,6 @@ Application::Application(const char* Name, uint32_t Width, uint32_t Height) :
 	m_Window.SetVsync(true);
 
 	// Setup ImGui
-	//IMGUI_CHECKVERSION( );
 	ImGui::CreateContext( );
 	ImGui::StyleColorsDark( );
 	ImGui_ImplGlfw_InitForOpenGL(m_Window, true);
@@ -87,6 +85,96 @@ void Application::Run( )
 		ImGUIDraw(modelView);
 		m_Window.Update( );
 	}
+}
+
+void Application::ImGUIDraw(std::shared_ptr<ModelPanel>& ModelView)
+{
+	static bool active = true;
+	ImGuiIO& io = ImGui::GetIO( );
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+	ImGui_ImplOpenGL3_NewFrame( );
+	ImGui_ImplGlfw_NewFrame( );
+	ImGui::NewFrame( );
+
+	ImGui::ShowDemoWindow( );
+
+	{//Docking
+
+		// Note: Switch this to true to enable dockspace
+		static bool dockspaceOpen = true;
+		bool opt_fullscreen = true;
+		static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+		// We are using the ImGuiWindowFlags_NoDocking flag to make the parent window not dockable into,
+		// because it would be confusing to have two docking targets within each others.
+		ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+		if(opt_fullscreen)		{
+			ImGuiViewport* viewport = ImGui::GetMainViewport( );
+			ImGui::SetNextWindowPos(viewport->Pos);
+			ImGui::SetNextWindowSize(viewport->Size);
+			ImGui::SetNextWindowViewport(viewport->ID);
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+			ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+			window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+			window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+		}
+
+		// When using ImGuiDockNodeFlags_PassthruCentralNode, DockSpace() will render our background and handle the pass-thru hole, so we ask Begin() to not render a background.
+		if(dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+			window_flags |= ImGuiWindowFlags_NoBackground;
+
+		// Important: note that we proceed even if Begin() returns false (aka window is collapsed).
+		// This is because we want to keep our DockSpace() active. If a DockSpace() is inactive, 
+		// all active windows docked into it will lose their parent and become undocked.
+		// We cannot preserve the docking relationship between an active window and an inactive docking, otherwise 
+		// any change of dockspace/settings would lead to windows being stuck in limbo and never being visible.
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+		ImGui::Begin("DockSpace Demo", &dockspaceOpen, window_flags);
+		ImGui::PopStyleVar( );
+
+		if(opt_fullscreen)
+			ImGui::PopStyleVar(2);
+
+		// DockSpace
+		if(io.ConfigFlags & ImGuiConfigFlags_DockingEnable)		{
+			ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+			ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+		}
+
+	}
+
+	if(ImGui::BeginMenuBar( ))		{
+		if(ImGui::BeginMenu("File")){
+
+			if(ImGui::MenuItem("New Scene")) std::cout << "New Scene\n";
+			if(ImGui::MenuItem("Import Object")) std::cout << "Import Object\n";
+			if(ImGui::BeginMenu("Export Scene")){
+				if(ImGui::MenuItem("Object")) std::cout << "obj\n";
+				if(ImGui::MenuItem("Image")) std::cout << "img\n";
+				ImGui::EndMenu( );
+			}
+			if(ImGui::MenuItem("Exit")) glfwSetWindowShouldClose(m_Window, GL_TRUE);
+			ImGui::EndMenu( );
+		}
+
+		ImGui::EndMenuBar( );
+	}
+
+	ImGui::Begin("Properties");
+	ModelView->Draw( );
+	ImGui::End( );
+
+	//ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail( );
+	//m_ViewportSize = {viewportPanelSize.x, viewportPanelSize.y};
+
+	//uint64_t textureID = m_Framebuffer->GetColorAttachmentRendererID( );
+	//ImGui::Image(reinterpret_cast<void*>(textureID), ImVec2{m_ViewportSize.x, m_ViewportSize.y}, ImVec2{0, 1}, ImVec2{1, 0});
+
+	ImGui::End( );// end dock
+
+	ImGui::Render( );
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData( ));
 }
 
 void Application::Mouse(GLFWwindow* Window, double Xpos, double Ypos)
@@ -163,23 +251,4 @@ void Application::ResizeWindow(GLFWwindow* window, int width, int height)
 	//sky.setProjection(projection);
 
 	glViewport(0, 0, width, height);
-}
-
-void Application::ImGUIDraw(std::shared_ptr<ModelPanel>& ModelView)
-{
-	static bool active = true;
-
-	ImGui_ImplOpenGL3_NewFrame( );
-	ImGui_ImplGlfw_NewFrame( );
-	ImGui::NewFrame( );
-
-	//ImGui::ShowDemoWindow( );
-
-	ImGui::Begin("Properties");
-	ModelView->Draw();
-	ImGui::End( );
-
-
-	ImGui::Render( );
-	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData( ));
 }
--- WorldBuilder/src/pch.h
+++ WorldBuilder/src/pch.h
@@ -21,7 +21,7 @@
 #include "OpenGL/Errors.h"
 #include "stb_image.h"
 
-#include <imgui.h>
+#include "imgui.h"
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
 
